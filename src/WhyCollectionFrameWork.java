5c6ff6fff7980b6e9dd57dec5064ab9ee7ddc62a
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2023-06-02 01:37:46 +0530

    WhyCollection

---
+++ src/WhyCollectionFrameWork.java
@@ -0,0 +1,41 @@
+/*
+Link : https://www.javatpoint.com/collections-in-java
+
+
+Data Structure  : It is way in by which we can store data in efficent way
+
+Primitive Data Structure : bit, byte, short, int,long, float,double , boolean,Char
+No-Primitive Data Structure :
+Linear Data Structure : String , Array, Stack , Queue, LinkedList
+Non-Linear Data Structure : Tree, SET , Graph
+
+We can use primitive data type for small application but for enterprise level we need No-Primitive Data Structure
+
+Why  Collection FrameWork :
+The Collection in Java is a framework that provides an architecture to store and manipulate the
+group of objects.
+Java Collections can achieve all the operations that you perform on a data such as searching,
+sorting, insertion, manipulation, and deletion.
+Java Collection means a single unit of objects. Java Collection framework provides many interfaces
+(Set, List, Queue, Deque) and classes (ArrayList, Vector, LinkedList, PriorityQueue, HashSet, LinkedHashSet, TreeSet)
+
+What is Collection in Java
+A Collection represents a single unit of objects, i.e., a group.
+
+What is a framework in Java
+It provides readymade architecture.
+It represents a set of classes and interfaces.
+It is optional.
+What is Collection framework
+The Collection framework represents a unified architecture for storing and manipulating a group of objects. It has:
+
+Interfaces and its implementations, i.e., classes
+Algorithm
+
+ */
+
+public class WhyCollectionFrameWork {
+    public static void main(String[] args) {
+
+    }
+}
