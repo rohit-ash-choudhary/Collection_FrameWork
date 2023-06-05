0313a6837ebca11385fdfbd19f2bdcf2732ea8db
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2023-06-06 03:54:03 +0530

    ArrayList

---
+++ src/ArrayList.java
@@ -0,0 +1,28 @@
+/*
+ArrayList :
+        1. ArrayList is a implemented  class that inherit the List Interface and list Interface
+        inherit Collection Interface.
+        2. Inside java.util package
+        3. Its implemented the List interface
+        Syntax
+            class arraylist implements List{
+            //Method
+            //Constructor
+            }
+         4. ArrayList was created bases on Growable Array(Resize Array).
+-- Properties of ArrayList:
+        1. ArrayList are index based data Structure.
+        2. ArrayList can stored hetrogenous data type
+        3. We can store duplicate data in ArrayList
+        4. ArrayList can store any number of Null Value.
+        5. ArrayList follow inseration order (same order insert , same order reterive )
+        6. ArrayList can't follow sorting order
+        7. ArrayList are non-synchronized data type
+        non-synchronized : if multiple thread allow to access at same time
+        synchronized : if multiple thread not allow ,its allow thread one by one
+
+
+*/
+public class ArrayList {
+
+}
