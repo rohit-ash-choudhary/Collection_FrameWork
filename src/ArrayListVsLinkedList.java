41583343c2c480eb0381253bb16b26150f58faf0
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2023-06-07 03:54:28 +0530

    ArrayListvsLinkedList

---
+++ src/ArrayListVsLinkedList.java
@@ -0,0 +1,19 @@
+/*
+Difference between ArrayList Vs LinkedList
+1. ArrayList worked on basics of List vs LinkedList acts as List and Deque
+2. ArrayList The underline data structure was growable(Resizable Array)
+   vs LinkedList The underline data structure was DoublyLinkedList and Circle List
+3. In  ArrayList Element as stored as continues memory location
+   vs LinkedList not stored in continuous memory location as nodes are there
+4. ArrayList is good in element retrieve operation
+   vs  LinkedList is good in Insertion and deletion operations
+5.
+
+
+ */
+
+public class ArrayListVsLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
