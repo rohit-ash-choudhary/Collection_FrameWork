477f8a5b9516dcc61b9d9292006cfe39be73c8f2
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2023-06-07 03:46:52 +0530

    LinkedList

---
+++ src/LinkedListMethodandConstructor.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class LinkedListMethodandConstructor {
+    public static void main(String[] args) {
+
+        LinkedList ll=new LinkedList();
+
+        ll.add(34);
+        ll.add("Rohtitash");
+        ll.add("Rohit");
+        ll.add("Rohit");
+        ll.add(null);
+        ll.add(3);
+
+
+
+        System.out.println(ll);
+/*
+        Interface Collection{
+            boolean add(Object obj);
+            boolean addAll(Collection c);
+            boolean remove(Object obj);
+            boolean removeAll(Collection c);
+
+            void clear();
+
+            boolean contains(Object obj);
+            boolean containsAll(Collection c);
+
+            boolean isEmpty();
+
+            int size();
+
+
+        }
+        as LinkedList inherit this interface all method availabe inside this */
+         LinkedList lt=new LinkedList();
+         lt.add(34);
+        lt.add(344);
+        lt.add(3432);
+
+
+        ll.add(4,5);
+        ll.get(4);
+        ll.addAll(lt);
+        ll.remove(5)/* remove element from particular index */
+
+        ll.set(3,5); /* set element 5 at index 3 */
+
+        ll.indexOf(45);
+
+        ll.lastIndexOf(5); /* in case duplicate element last index of element */
+
+
+        /* Contain List, Deque, Collection Method */
+
+        ll.addFirst(4);
+        ll.addLast(45);
+
+        ll.removeFirst();
+        ll.removeLast();
+        ll.removeFirstOccurrence(4);
+
+
+
+
+    }
+}
---
+++ src/LinkedListt.java
@@ -0,0 +1,38 @@
+/*
+   LinkedList :
+            LinkedList is a implemented class of List Interface , that present in Java.util.*;
+
+            Syntax :
+              LinkedList inherit List and Deque interface.
+              Class LinkedList Implements List, Deque (clone ,serializable too)
+              {
+                  //Constructor
+                  //Method
+              }
+           -- The Underline data structure of LinkedList is doubly Linkedlist and Circular LinkedList
+
+
+Disadvantage and Advantage of LinkedList :
+       Advantage of LinkedList
+                    - Insertion and Deletion was too fast but In Double Link list I am directory added the Node.
+                        (No need to shift element index like ArrayList)
+                    - We can traverse in both forward and backward Direction
+      Advantage of LinkedList :
+                    - it occupay extra space for previous node address
+                    - if Insertion and Deletion in between then handle previous and next node
+
+Properties of LinkedList :
+            - LinkedList was index based data structure.
+            - Store heterogeneous data types
+            - follow insertion order but not follow sorting order
+            - Stored duplicate elements
+            - LinkedList was non-synronized  data types
+            - No provide data consitant gaurantee.
+
+
+
+
+
+ */
+public class LinkedListt {
+}
